09cbb9d4d536287289dc716a0a102da55483cfdc
Ang31D <53995136+Ang31D@users.noreply.github.com>
2021-03-12 08:11:53 +0100

    Update search_code.py

--- search_code.py
+++ search_code.py
@@ -5,7 +5,7 @@ import binascii
 
 """
 // Usage:
-python2 deobfuscate.py -f <infile> [-o <outfile>]
+python2 deobfuscate.py -f <infile> [-o <outfile>] -s <search phrase>
 
 * example
 python2 deobfuscate.py -f data\kek.gay\setup.py -o data\kek.gay\obfuscated\setup.py
@@ -13,14 +13,10 @@ python2 deobfuscate.py -f data\kek.gay\setup.py -o data\kek.gay\obfuscated\setup
 
 parser = argparse.ArgumentParser(description='Deobfuscation.')
 parser.add_argument('-f', '--infile', dest="infile", required=True, help='Input file')
-parser.add_argument('-s', '--search', dest="search_for", help='search phrase')
+parser.add_argument('-s', '--search', dest="search_for", required=True, help='search phrase')
 parser.add_argument('-o', '--outfile', dest="outfile", default = '', help='Output file (optional)')
 parser.add_argument('-a', '--append', dest="out_append", action='store_true', help='Append to output file')
 parser.add_argument('-A', '--out-after', dest="out_after", type=int, default = 0, help='Out num of rows after search hit')
-parser.add_argument('-I', '--ioc', dest="out_ioc_only", action='store_true', help='Output IOC only (optional)')
-parser.add_argument('-S', '--script', dest="out_full_script", action='store_true', help='Output full script (optional)')
-parser.add_argument('-R', '--row', dest="include_row", action='store_true', help='Include row number (optional)')
-parser.add_argument('-q', '--quiet', dest="quiet", action='store_true', help='Suppress output')
 
 args = parser.parse_args()
 
@@ -102,51 +98,4 @@ for row in rows:
 				f.write('+[%s] : %s' % (row+1+i+1, file_content[row+i+1]))
 
 if len(args.outfile) > 0:
-	f.close()
-exit()
-
-# obfuscated data
-obf_list = fetch_data_by_regex(infile, pattern_xor_compressed)
-print("")
-file_content = open(infile, "r").read()
-
-if len(args.outfile) > 0:
-	f=open(args.outfile,"w")
-
-for obf in obf_list:
-	## extract the hex value
-	data = obf[obf.index('\\x'):obf.rindex('\\x')+4]
-	de_obf = deobfuscate(data)
-	# output to console
-	if not args.quiet:
-		if not args.out_ioc_only:
-			print(obf)
-			print(de_obf+'\n')
-		else:
-			if len(de_obf) > 0:
-				print(de_obf)
-
-	if de_obf != '':
-		# fix quotes around strings, default single quote.
-		quote_chr = "'"
-		if "'" in de_obf.strip():
-			quote_chr = '"'
-		if "\\'" in de_obf.strip():
-			quote_chr = "'"
-		if '\\"' in de_obf.strip():
-			quote_chr = '"'
-
-		if args.out_full_script:
-			file_content = file_content.replace(obf, quote_chr+de_obf.strip()+quote_chr)
-
-		if len(args.outfile) > 0 and not args.out_full_script:
-			if not args.out_ioc_only:
-				f.write("%s\n" % (obf))
-				f.write("%s\r\n" % (de_obf))
-			else:
-				f.write("%s\n" % (de_obf))
-
-if len(args.outfile) > 0:
-	if args.out_full_script:
-		f.write(file_content)
 	f.close()
